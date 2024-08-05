a789f0fbb59530f5cb62002cc73be1c8ed1f9954
MykytaOlenykov <115184331+MykytaOlenykov@users.noreply.github.com>
2024-08-05 20:52:35 +0000

    Deploying to gh-pages from @ MykytaOlenykov/goit-mcs-advancedjs-hw-03@67703795977f780f0e140f66d610c9d96c1af366 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,11 +1,11 @@
-var g=(s,t,a)=>{if(!t.has(s))throw TypeError("Cannot "+a)};var f=(s,t,a)=>(g(s,t,"read from private field"),a?a.call(s):t.get(s)),l=(s,t,a)=>{if(t.has(s))throw TypeError("Cannot add the same private member more than once");t instanceof WeakSet?t.add(s):t.set(s,a)};var h=(s,t,a)=>(g(s,t,"access private method"),a);import{S as P,i as w}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))o(r);new MutationObserver(r=>{for(const e of r)if(e.type==="childList")for(const n of e.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function a(r){const e={};return r.integrity&&(e.integrity=r.integrity),r.referrerPolicy&&(e.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?e.credentials="include":r.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function o(r){if(r.ep)return;r.ep=!0;const e=a(r);fetch(r.href,e)}})();const L=document.querySelector(".gallery"),A=new P(".gallery a",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});function E(s){L.innerHTML=s.map(R).join(""),A.refresh()}function R({largeImageURL:s,tags:t,webformatURL:a,likes:o,views:r,comments:e,downloads:n}){return`
+var g=(e,s,t)=>{if(!s.has(e))throw TypeError("Cannot "+t)};var f=(e,s,t)=>(g(e,s,"read from private field"),t?t.call(e):s.get(e)),l=(e,s,t)=>{if(s.has(e))throw TypeError("Cannot add the same private member more than once");s instanceof WeakSet?s.add(e):s.set(e,t)};var h=(e,s,t)=>(g(e,s,"access private method"),t);import{S as P,i as w}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))o(a);new MutationObserver(a=>{for(const r of a)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function t(a){const r={};return a.integrity&&(r.integrity=a.integrity),a.referrerPolicy&&(r.referrerPolicy=a.referrerPolicy),a.crossOrigin==="use-credentials"?r.credentials="include":a.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(a){if(a.ep)return;a.ep=!0;const r=t(a);fetch(a.href,r)}})();const L=document.querySelector(".gallery"),A=new P(".gallery a",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});function E(e){L.innerHTML=e.map(R).join(""),A.refresh()}function R({largeImageURL:e,tags:s,webformatURL:t,likes:o,views:a,comments:r,downloads:n}){return`
         <li class="gallery-item">
-            <a class="gallery-link" href="${s}">
+            <a class="gallery-link" href="${e}">
                 <img
                   class="gallery-image"
                   loading="lazy"
-                  src="${a}"
-                  alt="${t}"
+                  src="${t}"
+                  alt="${s}"
                 />
                 <span class="gallery-info-bar">
                     <span class="gallery-info">
@@ -14,11 +14,11 @@ var g=(s,t,a)=>{if(!t.has(s))throw TypeError("Cannot "+a)};var f=(s,t,a)=>(g(s,t
                     </span>
                     <span class="gallery-info">
                         <span class="gallery-info-label">Views</span>
-                        <span class="gallery-info-value">${r}</span>
+                        <span class="gallery-info-value">${a}</span>
                     </span>
                     <span class="gallery-info">
                         <span class="gallery-info-label">Comments</span>
-                        <span class="gallery-info-value">${e}</span>
+                        <span class="gallery-info-value">${r}</span>
                     </span>
                     <span class="gallery-info">
                         <span class="gallery-info-label">Downloads</span>
@@ -27,5 +27,5 @@ var g=(s,t,a)=>{if(!t.has(s))throw TypeError("Cannot "+a)};var f=(s,t,a)=>(g(s,t
                 </span>
             </a>
         </li>
-    `}const y=document.querySelector(".progress-bar");function m(s){s?y.classList.add("progress-bar--show"):y.classList.remove("progress-bar--show")}const d={error(s){w.error({position:"topRight",message:s})}},{VITE_API_KEY:O}={BASE_URL:"/goit-mcs-advancedjs-hw-03/",MODE:"production",DEV:!1,PROD:!0,SSR:!1};var i,c,u,b;class ${constructor(){l(this,u);l(this,i,"https://pixabay.com/api/");l(this,c,{key:O,q:"",image_type:"photo",orientation:"horizontal",safesearch:!0})}fetchPhotos({searchValue:t="",startCallback:a=null,finallyCallback:o=null}){h(this,u,b).call(this,t);const r=new URLSearchParams({...f(this,c),q:t});a==null||a(),m(!0),fetch(`${f(this,i)}?${r}`,{method:"GET"}).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>{var n;(n=e.hits)!=null&&n.length||d.error("Sorry, there are no images matching your search query. Please try again!"),E(e.hits??[])}).catch(e=>{(e==null?void 0:e.name)!=="AbortError"&&d.error((e==null?void 0:e.message)??"Something went wrong")}).finally(()=>{o==null||o(),m(!1)})}}i=new WeakMap,c=new WeakMap,u=new WeakSet,b=function(t){if(t.length>100){const a="Search value may not exceed 100 characters.";throw new Error(a)}};const q=new $,v=document.querySelector(".search-form"),S=v.querySelector(".button");v.addEventListener("submit",D);let p=!1;function D(s){var a;if(s.preventDefault(),p)return;const t=((a=s.target.elements["search-value"])==null?void 0:a.value)??"";q.fetchPhotos({searchValue:t.trim(),startCallback:()=>{p=!0,S.setAttribute("disabled","")},finallyCallback:()=>{p=!1,S.removeAttribute("disabled")}})}
+    `}const y=document.querySelector(".progress-bar");function d(e){e?y.classList.add("progress-bar--show"):y.classList.remove("progress-bar--show")}const m={error(e){w.error({position:"topRight",message:e})}},{VITE_API_KEY:O}={BASE_URL:"/goit-mcs-advancedjs-hw-03/",MODE:"production",DEV:!1,PROD:!0,SSR:!1};var i,c,u,b;class ${constructor(){l(this,u);l(this,i,"https://pixabay.com/api/");l(this,c,{key:O,q:"",image_type:"photo",orientation:"horizontal",safesearch:!0})}fetchPhotos({searchValue:s="",startCallback:t=null,finallyCallback:o=null}){h(this,u,b).call(this,s);const a=new URLSearchParams({...f(this,c),q:s});t==null||t(),d(!0),fetch(`${f(this,i)}?${a}`,{method:"GET"}).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{var n;(n=r.hits)!=null&&n.length||m.error("Sorry, there are no images matching your search query. Please try again!"),E(r.hits??[])}).catch(r=>{m.error((r==null?void 0:r.message)??"Something went wrong")}).finally(()=>{o==null||o(),d(!1)})}}i=new WeakMap,c=new WeakMap,u=new WeakSet,b=function(s){if(s.length>100){const t="Search value may not exceed 100 characters.";throw new Error(t)}};const q=new $,v=document.querySelector(".search-form"),S=v.querySelector(".button");v.addEventListener("submit",D);let p=!1;function D(e){var t;if(e.preventDefault(),p)return;const s=((t=e.target.elements["search-value"])==null?void 0:t.value)??"";q.fetchPhotos({searchValue:s.trim(),startCallback:()=>{p=!0,S.setAttribute("disabled","")},finallyCallback:()=>{p=!1,S.removeAttribute("disabled")}})}
 //# sourceMappingURL=commonHelpers.js.map
